eead4de80ce024c64e67c91420255b9b1dfbd0b6
Breno-Morais-Palomaro <brmoraispalo@gmail.com>
2025-06-11 15:53:04 -0300

    Deu errado

--- 12_CustomSerialization/src/main/java/br/com/brenomp/repositories/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.brenomp.repositories;
-
-import br.com.brenomp.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
